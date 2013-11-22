2b6f61b86ce3f79726cc1a42c663d952168cc378
Steveo1485 <stevennugent@gmail.com>
2013-11-21 21:56:04 -0800

    wip: solveBoard not working yet

--- spec/solver_spec.js
+++ spec/solver_spec.js
@@ -8,65 +8,73 @@ beforeEach(function(){
 
 describe("checkString", function(){
   it("checks the string for 0s", function(){
-    expect(checkString(unsolvedBoard)).toBeTruthy()
+    expect(Board.checkString(unsolvedBoard)).toBeTruthy()
   })
 
   it("returns false if no 0s", function(){
-    expect(checkString(solvedBoard)).toBeFalsy()
+    expect(Board.checkString(solvedBoard)).toBeFalsy()
   })
 })
 
 describe("splitBoard", function() {
   it("splits the string in a 9x9 array", function() {
-    expect(splitBoard(solvedBoard)).toEqual(splittedededBoard)
+    expect(Board.splitBoard(solvedBoard)).toEqual(splittedededBoard)
   })
 })
 
 describe("whichRow", function(){
   it("returns the row index of a particular cell", function(){
-    expect(whichRow(11)).toEqual(1)
+    expect(Board.whichRow(11)).toEqual(1)
   })
 })
 
 describe("whichColumn", function() {
   it("returns the column of a particular cell", function() {
-    expect(whichColumn(13)).toEqual(4)
+    expect(Board.whichColumn(13)).toEqual(4)
   })
 })
 
 describe("whichBox", function(){
   it("returns the box that a particular cell is in", function(){
-    expect(whichBox(13)).toEqual(1)
+    expect(Board.whichBox(13)).toEqual(1)
   })
 })
 
-describe("get current row", function(){
+describe("getRow", function(){
   it("returns the array associated with a particular row", function(){
-    expect(getRow(splittedededBoard, 11)).toEqual(['1','2','3','4','5','6','7','8','9'])
+    expect(Board.getRow(splittedededBoard, 11)).toEqual(['1','2','3','4','5','6','7','8','9'])
   })
 })
 
-describe("get current column", function() {
+describe("getColumn", function() {
   it("returns the column array associated with the current cell", function() {
-    expect(getColumn(splittedededBoard, 9)).toEqual(['1','1','1'])
+    expect(Board.getColumn(splittedededBoard, 9)).toEqual(['1','1','1'])
   })
 })
 
-describe("get current box", function(){
+describe("getBox", function(){
   it("returns the box associated with the current cell", function(){
-    expect(getBox(splittedededBoard, 0)).toEqual(['1','2','3','1','2','3','1','2','3'])
+    expect(Board.getBox(splittedededBoard, 2)).toEqual(['1','2','3','1','2','3','1','2','3'])
   })
 })
 
 describe("find the first zero aka empty cell", function() {
   it("returns the index of the first empty cell", function() {
-    expect(getFirstZero(unsolvedBoard)).toEqual(2)
+    expect(Solver.getFirstZero(unsolvedBoard)).toEqual(2)
   })
 })
 
 describe("solveCell", function(){
-  var unsolvedSplitBoard = splitBoard("530678912672195348198342567")
+  var unsolvedSplitBoard = Board.splitBoard("530678912672195348198342567")
   it("replaces empty cell with value that doesn't exist in it's row, column, or box", function(){
-    expect(solveCell(unsolvedSplitBoard,2)).toEqual('4')
+    expect(Solver.solveCell(unsolvedSplitBoard,2)).toEqual('4')
+  })
+})
+
+describe("solveBoard", function() {
+  var superUnsolvedSplitBoard = Board.splitBoard("609238745274561398853947621486352179792614583531879264945723816328196457167485932")
+
+  it("solves the Sudoku board until no zeroes are left", function() {
+    expect(Solver.solveBoard(superUnsolvedSplitBoard)).toEqual("619238745274561398853947621486352179792614583531879264945723816328196457167485932")
   })
 })
--- src/solver.js
+++ src/solver.js
@@ -1,87 +1,108 @@
-function checkString(string) {
-  if (string.indexOf("0") === -1) {
-    return false
-  } else {
-    return true
-  }
-}
+Board = {
+  //passed in string
+  checkString: function(string) {
+    if (string.indexOf("0") === -1) {
+      return false
+    } else {
+      return true
+    }
+  },
 
-function splitBoard(board){
-  boardRows = board.match(/\d{9}/g)
-  splittedBoard = []
-  for(i=0; i<boardRows.length; i++){
-    splittedBoard.push(boardRows[i].split(""))
-  }
-  return splittedBoard
-}
+  //passed in string
+  splitBoard: function(board){
+    boardRows = board.match(/\d{9}/g)
+    splittedBoard = []
+    for(i=0; i<boardRows.length; i++){
+      splittedBoard.push(boardRows[i].split(""))
+    }
+    return splittedBoard
+  },
 
-function whichRow(cell) {
-  return Math.floor(cell/9)
-}
+  //passed in index
+  whichRow: function(cell) {
+    return Math.floor(cell/9)
+  },
 
-function whichColumn(cell){
-  return (cell%9)
-}
+  //passed in index
+  whichColumn: function(cell){
+    return (cell%9)
+  },
 
-function whichBox(cell){
-  var row = whichRow(cell)
-  var column = whichColumn(cell)
-  return 3*(Math.floor(row/3))+(Math.floor(column/3))
-}
+  //passed in index
+  whichBox: function(cell){
+    var row = Board.whichRow(cell)
+    var column = Board.whichColumn(cell)
+    return (3*(Math.floor(row/3)))+(Math.floor(column/3))
+  },
 
-function getRow(board, cell) {
-  var rowNumber = whichRow(cell)
-  return board[rowNumber]
-}
+  //passed in split board & index
+  getRow: function(board, cell) {
+    var rowNumber = Board.whichRow(cell)
+    return board[rowNumber]
+  },
 
-function getColumn(board, cell){
-  var columnNumber = whichColumn(cell)
-  var column = []
-  for(i=0; i<board.length; i++){
-    column.push(board[i][columnNumber])
-  }
-  return column
-}
+  //passed in split board & index
+  getColumn: function(board, cell){
+    var columnNumber = Board.whichColumn(cell)
+    var column = []
+    for(i=0; i<board.length; i++){
+      column.push(board[i][columnNumber])
+    }
+    return column
+  },
 
-function getBox(board, cell){
-  var boxNum = whichBox(cell)
-  var box = []
-  for(i=0; i<board.length; i++){
-    for(j=0; j<board[i].length; j++){
-      if(whichBox(j) === boxNum){
-        box.push(board[i][j])
+  //passed in split board & index
+  getBox: function(board, cell){
+    var boxNum = Board.whichBox(cell)
+    var box = []
+    for(i=0; i<board.length; i++){
+      // console.log(board[i])
+      for(j=0; j<board[i].length; j++){
+
+        if(Board.whichBox(j) === boxNum){
+          // console.log(Board.whichBox(j))
+          box.push(board[i][j])
+        }
       }
     }
+    return box
   }
-  return box
 }
 
-function getFirstZero(board){
-  return board.indexOf("0")
+Solver = {
+  //passed in array
+  getFirstZero: function(row){
+    return row.indexOf("0")
+  },
+
+  //passed in split board & index
+  solveCell: function(board, cell) {
+    var row = Board.getRow(board, cell)
+    var column = Board.getColumn(board, cell)
+    var box = Board.getBox(board, cell)
+    var solvedRow = ['1','2','3','4','5','6','7','8','9']
+    var diffedRow = solvedRow.diff(row).diff(column).diff(box)
+    if(diffedRow.length === 1){
+      return diffedRow[0]
+    }else {
+      return false
+    }
+  },
+
+  //passed in split board
+  solveBoard: function(board){
+    for(i=0; i<board.length; i++){
+      var emptyCell = Solver.getFirstZero(board[i])
+      // console.log(emptyCell)
+      // console.log(Solver.solveCell(board,emptyCell))
+      board[i][emptyCell] = Solver.solveCell(board,emptyCell)
+    }
+    return board.join(",").replace(/,/g,"")
+  }
 }
 
 Array.prototype.diff = function(array){
   return this.filter(function(i) {
     return !(array.indexOf(i) > -1)
   })
-}
-
-function solveCell(board, cell) {
-  var row = getRow(board, cell)
-  var column = getColumn(board, cell)
-  var box = getBox(board, cell)
-  var solvedRow = ['1','2','3','4','5','6','7','8','9']
-  var diffedRow = solvedRow.diff(row)
-  diffedRow = diffedRow.diff(column)
-  diffedRow = diffedRow.diff(box)
-  if(diffedRow.length > 0){
-    return diffedRow[0]
-  }else {
-    return false
-  }
-}
-
-function solveBoard(board){
-  var emptyCell = getFirstZero(board)
-  solveCell(emptyCell)
 }
